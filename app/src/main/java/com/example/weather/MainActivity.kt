9e60d17cda0a13b4ad353a51843dffcd2ccf89ea
Kaz1deX <max2003255@gmail.com>
2024-01-02 17:02:01 +0300

    Commit

---
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.weather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.weather.screens.MainCard
+import com.example.weather.screens.TabLayout
+import com.example.weather.ui.theme.WeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherTheme {
+                Image(
+                    painter = painterResource(id = R.drawable.weather),
+                    contentDescription = "weather1",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.8f),
+                    contentScale = ContentScale.FillBounds
+                )
+                Column {
+                    MainCard()
+                    TabLayout()
+                }
+            }
+        }
+    }
+}
